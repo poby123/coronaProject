e970d79e95367743d37ba0f663e7c829aa765df4
poby123 <youngsm12@naver.com>
2020-10-10 21:50:35 +0900

    unused function delete

--- RasberryController.py
+++ RasberryController.py
@@ -1,15 +1,6 @@
 import time
 from multiprocessing import Value
 
-# def getNameByNFC(id):
-#     time.sleep(3)
-#     dto = {}
-#     dto['type'] = 'NFC_NAME'
-#     dto['uid'] = 1234
-#     dto['name'] = DataController.getNameByNFC(dto['uid'])
-#     dto['temp'] = None
-#     return dto
-
 def getNFCId(interrupt):
     for i in range(3):
         if(interrupt.value):
@@ -23,4 +14,4 @@ def getTemp(interrupt):
         if(interrupt.value):
             return 'INTERRUPTED'
         time.sleep(1)
-    return 37.3
+    return 37.2
