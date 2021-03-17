9de404f359621cfea1819040b5e213bd7e72d7b0
poby123 <youngsm12@naver.com>
2021-03-17 10:58:08 +0900

    test code without raspberrypi update

--- RaspberryController.py
+++ RaspberryController.py
@@ -60,41 +60,36 @@ class RaspberryController():
 
 # For Test Code ↓↓
 # import time
+# import random
 # from multiprocessing import Value
-
 
 # class RaspberryController():
 #     def __init__(self, interrupt):
 #         self.interrupt = interrupt
 
-#     def __del__(self):
-#         pass
-
 #     def getNFCId(self):
-#         try:
-#             time.sleep(1)
-#             id = 138164
-#         finally:
-#             pass
-#         return id
+#         nfc_numbers = [584187407785, 584186083456, 584184898698, 584183666907]
+#         return random.choice(nfc_numbers)
 
 #     def getTemp(self):
-#         n = 4
 #         total = 0
+#         n = 4
+#         while(self.getDistance() >= 8.0):
+#             time.sleep(0.2)
 #         for i in range(n):
-#             total += 36.52
+#             total += float(random.uniform(36.0, 38.5))
 #             if(self.interrupt.value):
 #                 return 'INTERRUPTED'
 #             time.sleep(0.5)
 #         return round(total / n, 2)
+
+#     def getDistance(self):
+#         return 2.0
 
 
 if __name__ == '__main__':
     v = Value('b', False)
     con = RaspberryController(v)
     print(con.getNFCId())
-    # for i in range(100):
-    # time.sleep(1)
-    # res = con.getTemp()
-    # res = con.getNFCId()
-    # print(res)
+    print(con.getTemp())
+    print(con.getDistance())
